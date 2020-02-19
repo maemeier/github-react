686c7a9bffeae9aaab23474416f9604400655f7f
Mae Kangsadan <maemeier@live.com>
2020-02-19 10:12:13 +0100

    change to function component

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,15 +1,14 @@
 import React, { Component } from "react";
 
-class Navbar extends Component {
-  render() {
-    return (
-      <nav className="navbar bg-primary">
-        <h1>
-          <i className={this.props.icon} />
-          {this.props.title}
-        </h1>
-      </nav>
-    );
-  }
-}
+const Navbar = ({ icon, title }) => {
+  return (
+    <nav className="navbar bg-primary">
+      <h1>
+        <i className={icon} />
+        {title}
+      </h1>
+    </nav>
+  );
+};
+
 export default Navbar;
